6c5297d882b2137d39662ba2e64122177e19be1e
Ocksana Bondarenko <bondarrren777@gmail.com>
2022-10-12 10:48:19 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerID = null;
 
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+  console.log('Start coloring!');
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  timerID = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onBtnStopClick() {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  clearInterval(timerID);
+  console.log('Stop coloring!');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
